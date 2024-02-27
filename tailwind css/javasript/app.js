a521c2d52a6fc7cb5fa49183a23e3b06c1d91d2a
atifarif0084 <atifarif0084@gmail.com>
2024-02-27 10:05:45 +0500

    need pratice

---
+++ tailwind css/javasript/app.js
@@ -0,0 +1,179 @@
+// alert('wait!')
+
+// var a='karachi karachi hain mera bhai'
+
+// alert(a)
+
+// var a = 'atif arif'
+// var a = 'arif latif'
+// alert(a)
+
+// var b ="abc"
+// b = 123
+// b = "pakistan zindabad"
+// alert(b)
+// console.log(b)
+// var a = 10
+// var b = a + 30
+// console.log(b)
+// var a = 10*(5+30)
+// console.log(a)
+// var a= 30
+// var b= "20"
+// var c = a+b
+// console.log(c)
+// var a = "Atif"
+// var b = "Arif"
+// var c = a + " " + b
+// console.log(c)
+
+// 22-2-2024
+// pre-increment
+// var a = 10
+// var b = ++a
+// console.log(b)
+
+// Post-increment
+// var a = 10
+// var b = a++
+// console.log(b)
+// console.log(a)
+
+// user value
+// var a = prompt("Enter your First Name")
+// var b = prompt("Enter your last Name")
+// console.log(a,b)
+// var a = + prompt ("Enter your Math marks")
+// var b = + prompt ("Enter your English marks")
+// var c = + prompt ("Enter your Urdu marks")
+// var d = + prompt ("Enter your ISlamiat marks")
+// var e = + prompt ("Enter your Science marks")
+// console.log(a,b,c,d,e)
+
+// var total =(a+b+c+d+e)
+// console.log(total)
+
+
+// legal and illegal varibale name
+
+// var asd = '' legal
+// var ad12 = '' legal
+// var ab_ = '' legal 
+// var var =  '' illegal
+// var 123 =  '' illegal
+// var ab#$% =  '' illegal
+// var  =  '' illegal
+
+// if statement
+// var a = 256*512
+// var b = 16*1024
+
+// if(a == b){
+//     console.log("a is equal to b to u can do action")
+// }else{
+//     console.log("a is not equal to b to u can do action")
+// }
+// // Example
+//  var a = 10
+//  var b = 20
+
+//  if(a == b){
+//     console.log("submit")
+// }else{
+// console.log("not submitted")
+// }
+
+// var javad = 70
+// var umar = 80
+// var ali = 90
+// var fahad = 60
+// if(javad > 70){
+//     console.log("Methai")
+    
+// }else{
+//     console.log("dhulai")
+// }
+// if(umar> 70){
+//     console.log("Methai")
+    
+// }else{
+//     console.log("dhulai")
+// }
+// if(javad,umar,ali,fahad > 70){
+//     console.log("Methai")
+    
+// }else{
+//     console.log("dhulai")
+// }
+// if(javad,umar,ali,fahad > 70){
+//     console.log("Methai")
+    
+// }else{
+//     console.log("dhulai")
+// }
+
+// var a = +prompt("Enter your Math marks")
+// var b = +prompt("Enter your English marks")
+// var c = +prompt("Enter your Urdu marks")
+// var d = +prompt("Enter your Islamiat marks")
+// var e = +prompt("Enter your Science marks")
+
+// var total =(a+b+c+d+e)/5
+// alert (total)
+
+// if(total > 70){
+//     console.log("A+")
+// }else if(total > 60){
+//     console.log("A")
+// }
+// else if(total > 50){
+//     console.log("B")
+// }
+// else if(total > 40){
+//     console.log("C")
+// }
+// else if(total > 30){
+//     console.log("F")
+// }
+
+// 24-02-24
+// nasted way
+// var studentCard = false
+// var dept = "Sci"
+
+// if(studentCard == true){
+//     if(dept =="Sci")
+//     console.log("Allow")
+// }else {
+//     console.log("Not Allow")
+// }
+
+
+// // Both condition must be ture
+// if(studentCard == true && dept == "Sci"){
+//     console.log("Allow")
+// }
+// else{
+//     console.log("Not Allow")
+// }
+
+
+// one condition must be ture
+// if(studentCard == true || dept == "Sci"){
+//     console.log("Allow")
+// }
+// else{
+//     console.log("Not Allow")
+// }
+
+// for adding multiple value
+var students =["Zaid","Haris","Fazal"]
+
+
+// for adding value in end 
+// students.push("Arsalan")
+// students.pop()
+
+// for adding value in start
+students.
+console.log(students)
